0c29aab346201fcc461a3579a441fa6c21201321
Bekbolat-programmer <75240128+Bekbolat-programmer@users.noreply.github.com>
2020-12-11 11:33:10 +0600

    Add files via upload

--- школа.py
+++ школа.py
@@ -1,23 +1,15 @@
-def shift_letter(letter, shift):
-    letter_new = ord(letter) - ord('a')
-    shifted_letter = (letter_new + shift) % 26
-    return chr(ord('a') + shifted_letter)
-
-
-def lable_transformation(lable, shift):
-    result = ''
-    for letter in lable:
-        if 'a' <= letter.lower() <= 'z':
-            if letter.islower():
-                result += shift_letter(letter, shift)
-            else:
-                result += shift_letter(letter.lower(), shift).upper()
-        else:
-            result += letter
-    return result
-
-
-lable = input('Введите строку для приобразования: ')
-shift = int(input('Введите сдвиг: '))
-lable_new = lable_transformation(lable, shift)
-print(lable_new)
+fin = open('f1.txt', 'r')
+fout = open('f2.txt', 'w')
+fin_st = fin.read().split('\n')
+a, b = [], []
+for i in range(len(fin_st)):
+    if i % 2 == 0:
+        a.append(fin_st[i])
+    else:
+        b.append(fin_st[i])
+b.sort(reverse=True)
+a.sort()
+for i in a:
+    fout.write("{:s}\n".format(i))
+for i in b:
+    fout.write("{:s}\n".format(i))
