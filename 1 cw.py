d7ee112bb5612d02d8f69525055001ac29798060
Bekbolat-programmer <75240128+Bekbolat-programmer@users.noreply.github.com>
2020-12-25 09:25:22 +0600

    Add files via upload

--- 1 cw.py
+++ 1 cw.py
@@ -1,41 +1,17 @@
-t = input('Введите текст: ')
-r = len(t) ** 0.5
-if float(round(r)) == r:
-    n = round(r)
+a, b, c = int(input('Введите стороны прямоугольника(через enter): ')), int(input()), int(input())
+a = [a, b, c]
+a.sort()
+if a[0] ** 2 == a[2] ** 2 - a[1] ** 2:
+    if a[1] ** 2 == a[2] ** 2 - a[0] ** 2:
+        if a[2] ** 2 == a[0] ** 2 + a[1] ** 2:
+            b = 1
+        else:
+            b = 0
+    else:
+        b = 0
 else:
-    n = int(r) + 1
-end = [[" "] * n for i in range(n)]
-rt = []
-a = n
-for i in range(n):
-    end[i][0] = t[i]
-for i in range(n - 1, 0, -1):
-    for j in range(2):
-        rt.append(t[a:a + i])
-        a += i
-ot = 2
-sh = [1, 0, 0, 0]
-for i in range(0, len(rt)):
-    if ot == 1:
-        for j in range(len(rt[i])):
-            end[sh[ot - 1] + j][sh[ot - 1]] = rt[i][j]
-        sh[ot - 1] = sh[ot - 1] + 1
-        ot += 1
-    elif ot == 2:
-        for j in range(len(rt[i])):
-            end[n - sh[ot - 2]][1 + j + sh[ot - 1]] = rt[i][j]
-        sh[ot - 1] = sh[ot - 1] + 1
-        ot += 1
-    elif ot == 3:
-        for j in range(len(rt[i])):
-            end[n - 1 - sh[ot - 2] - j][n - 1 - sh[ot - 1]] = rt[i][j]
-        sh[ot - 1] = sh[ot - 1] + 1
-        ot += 1
-    elif ot == 4:
-        for j in range(len(rt[i])):
-            end[sh[ot - 1]][n - 1 - sh[ot - 2] - j] = rt[i][j]
-        sh[ot - 1] = sh[ot - 1] + 1
-        ot = 1
-for i in range(n - 1):
-    print(''.join(end[i]), end="")
-print(''.join(end[n - 1]))
+    b = 0
+if b == 1:
+    print('Треугольник является прямоугольным')
+else:
+    print('Треугольник не прямоугольный')
